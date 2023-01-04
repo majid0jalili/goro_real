f6900f39523a705bef10bd9c246e4cc32bc7dca3
Majid <majid@utexas.edu>
2023-01-04 19:41:16 +0000

    added forceful set

--- prefetcher.py
+++ prefetcher.py
@@ -15,29 +15,33 @@ class Prefetcher():
             for pf in range(num_pf_per_core):
                 setting.append(0)
             self.last_setting.append(setting)
-        self.all_prefetcher_set(self.last_setting)
+        self.all_prefetcher_set_forced()
 
     def prefetcher_set(self, core, mask):
         int_mask = int("".join(str(i) for i in mask), 2)
+        print("int_mask", int_mask)
         cmd = "sudo wrmsr 0x1a4 -p "+str(core)+" "+str(int_mask)
-        # print("Running cmd ", cmd)
         process = subprocess.Popen(cmd,
                                    stdout=subprocess.PIPE,
                                    stderr=subprocess.PIPE,
                                    shell=True)
         out, err = process.communicate()
+        print(out)
 
+    def all_prefetcher_set_forced(self):
+        for core in range(self.num_cpu):
+            self.prefetcher_set(2*core, "0")
+        
+        
+        
     def all_prefetcher_set(self, mask):
         acc = []
-        ch = 0
         for core in range(self.num_cpu):
             if collections.Counter(mask[core]) != collections.Counter(self.last_setting[core]):
-                ch += 1
                 self.prefetcher_set(2*core, mask[core])
             for m in mask[core]:
                 acc.append(m)
             self.last_setting[core] = mask[core]
-        # print("Cores chnaged ", ch)
         return acc
 
     def all_prefetchers_on(self):
